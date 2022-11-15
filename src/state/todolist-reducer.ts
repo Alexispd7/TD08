6ecbbabad90b2e096319009d990c97c7749e2413
alexispd7 <alexispd7@gmail.com>
2022-11-16 00:26:17 +0500

    com_test_53

--- src/state/todolist-reducer.ts
+++ src/state/todolist-reducer.ts
@@ -11,18 +11,8 @@ type ActionType = {
 export const todolistReducer = (state: Array<TodolistType>, action: ActionType): Array<TodolistType> => {
     switch (action.type) {
         case 'INCREMENT-AGE':
-            let newState = {...state}
-            newState.age = state.age + 1
-            return newState
-        case 'INCREMENT-CHILDREN-COUNT':
-            //state.childrenCount = state.childrenCount + 1
-            return {
-                ...state, childrenCount: state.childrenCount + 1
-            }
-        case 'CHANGE-NAME':
-            return {
-                ...state, name: action.newName}
 
+            return state
 
         default:
             throw new Error('I don\'t understand this type')
