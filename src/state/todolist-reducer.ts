50a588f546705c6879e09f5f6820c3378b9eeb69
alexispd7 <alexispd7@gmail.com>
2022-11-15 23:28:53 +0500

    com_test_52

--- src/state/todolist-reducer.ts
+++ src/state/todolist-reducer.ts
@@ -1,9 +1,5 @@
+import {TodolistType} from "../App";
 
-type StateType = {
-    age: number
-    childrenCount: number
-    name: string
-}
 type ActionType = {
     type: string
         [key: string]: any
@@ -12,7 +8,7 @@ type ActionType = {
 // меня вызовут и дадут мне стейт (почти всегда объект)
 // и инструкцию (action, тоже объект)
 // согласно прописанному type в этом action (инструкции) я поменяю state
-export const todolistReducer = (state: StateType, action: ActionType) => {
+export const todolistReducer = (state: Array<TodolistType>, action: ActionType): Array<TodolistType> => {
     switch (action.type) {
         case 'INCREMENT-AGE':
             let newState = {...state}
